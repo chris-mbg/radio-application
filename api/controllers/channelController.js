4c097d735ba26d09a1678e0062a2c0198fa6d7ab
Christina <chrasse@hotmail.com>
2021-04-21 20:03:16 +0200

    Changed response and corrected some spelling mistakes.

--- api/controllers/channelController.js
+++ api/controllers/channelController.js
@@ -1,29 +1,31 @@
-const fetch = "node-fetch";
+const fetch = require("node-fetch");
 
 const jsonFormat = "format=json";
 const paginationFalse = "pagination=false";
 
-const covertToDateObject = require("../core/convertToDateObject");
+const convertToDateObject = require("../core/convertToDateObject");
 
 const getAllChannels = async (req, res) => {
   let channelsList = await fetch(
     `http://api.sr.se/api/v2/channels?${jsonFormat}&${paginationFalse}`
   );
   channelsList = await channelsList.json();
-  res.json(channelsList);
+
+  res.json(channelsList.channels);
+  // The response is now just the array with all the channels objects.
 };
 
 const getChannelById = async (req, res) => {
   let channel = await fetch(
-    `http://api.sr.se/api/v2/channels/${req.params.channelId}?${json}`
+    `http://api.sr.se/api/v2/channels/${req.params.channelId}?${jsonFormat}`
   );
   channel = await channel.json();
-  res.json(channel);
+  res.json(channel.channel);
 };
 
 const getChannelSchedule = async (req, res) => {
   let channelSchedule = await fetch(
-    `http://api.sr.se/api/v2/scheduledepisodes?${json}&${paginationFalse}&channelId=${req.params.channelId}&date=${req.query.date}`
+    `http://api.sr.se/api/v2/scheduledepisodes?${jsonFormat}&${paginationFalse}&channelId=${req.params.channelId}&date=${req.query.date}`
   );
   channelSchedule = await channelSchedule.json();
 
