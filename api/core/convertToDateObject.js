8249b5922af81bf6739b99d6828fd9843eacf5a5
Christina <chrasse@hotmail.com>
2021-04-21 09:43:55 +0200

    Added functions to encrypt passwords and convert date

---
+++ api/core/convertToDateObject.js
@@ -0,0 +1,7 @@
+const convertToDateObject = (SRTimeString) => {
+  return new Date(
+    parseInt(SRTimeString.replace(/[\/\(\)date]/gi, ""))
+  ).toLocaleString();
+};
+
+module.exports = convertToDateObject;
