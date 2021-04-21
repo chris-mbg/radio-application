beabccf202a1d1f67faa3f5ff0f9a97f4884056a
Christina <chrasse@hotmail.com>
2021-04-21 12:02:29 +0200

    Activated session

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const userPrefix = "/api/v1/users"
 const channelRoutes = require("./routes/channelRoutes.js");
 // const programRoutes = require("./routes/programRoutes");
 // const episodeRoutes = require("./routes/episodeRoutes");
-// const userRoutes = require("./routes/userRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -23,21 +23,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(session({
-//   secret: secret.secretKey,
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: {
-//     secure: "auto",
-//     maxAge: 1000 * 60 * 15    // Expires after 15 min, with this setting
-//   }
-// }));
+app.use(session({
+  secret: secret.secretKey,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: "auto",
+    maxAge: 1000 * 60 * 15    // Expires after 15 min, with this setting
+  }
+}));
 
-app.use(channelPrefix, channelRoutes);
+//app.use(channelPrefix, channelRoutes);
 // app.use(programPrefix, programRoutes);
 // app.use(episodePrefix, episodeRoutes);
-// app.use(userPrefix, userRoutes);
+app.use(userPrefix, userRoutes);
 
+// The build folder is now avaliable for the backend. Can run the application via the backend
+//app.use(express.static(path.join(__dirname, '../build')));
 
 app.listen(port, err => {
   err ? console.log('Server could not start, error: ', err) : console.log('Listening on port: ', port);
