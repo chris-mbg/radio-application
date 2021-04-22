a1ecfc9c0fa01a4fdf69c12a945e2ed2c56bc6e6
Christina <chrasse@hotmail.com>
2021-04-22 13:55:58 +0200

    Revert "Merge branch 'dev' into BE/validateEmail&Pw"
    This reverts commit 09e2c7a0eaf9cd87312d74aaf40365c0342b5b74.

--- api/routes/categoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const categoryController = require('../controllers/categoryController');
-
-router.get("/", categoryController.getAllCategories)
-router.get("/:categoryId", categoryController.getAllProgramsInCategory)
-
-module.exports = router;
--- api/routes/episodeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const episodeController = require('../controllers/episodeController');
-
-router.get("/:programId", episodeController.getAllEpisodesForProgram)
-//router.get("/:episodeId", episodeController.getAllProgramsInCategory)
-
-module.exports = router;
