7097454da9d259c5471ed0c80a371e3a72e78dd3
Christina <chrasse@hotmail.com>
2021-04-22 20:27:17 +0200

    Login and logout funcs

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,9 +1,10 @@
 import { createContext, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 export const UserContext = createContext();
 
 const UserContextProvider = props => {
-
+  const history = useHistory();
   const [userLoggedIn, setUserLoggedIn] = useState(null);
 
   const loggedInCheck = async () => {
@@ -15,8 +16,36 @@ const UserContextProvider = props => {
     }
   };
 
+  const logInUser = async userInput => {
+    let result = await fetch("/api/v1/users/login", {
+      method: 'POST',
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(userInput)
+    });
+    result = await result.json();
+    if (result.error) {
+      alert('Wrong email or password!');
+    } else if(result.success) {
+      loggedInCheck();
+    }
+  };
+
+  const logoutUser = async () => {
+    let result = await fetch("/api/v1/users/logout");
+    result = await result.json();
+    if (result.success) {
+      console.log('User logged out');
+      setUserLoggedIn(null);
+      history.push("/");
+    }
+  };
+
   const values = {
-    userLoggedIn
+    userLoggedIn,
+    logInUser,
+    logoutUser,
   }
 
   return (
