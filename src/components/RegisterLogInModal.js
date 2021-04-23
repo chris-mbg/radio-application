88924c048e53dfc4a2879bd7014c9e729179a617
Christina <chrasse@hotmail.com>
2021-04-23 12:49:25 +0200

    Added modal functionality to Navbar

--- src/components/RegisterLogInModal.js
+++ src/components/RegisterLogInModal.js
@@ -1,11 +1,11 @@
-import { useContext, useState } from "react";
 import { createPortal } from "react-dom";
+import styles from "../css/RegisterLogInModal.module.css";
 
 const RegisterLogInModal = ({ isVisible, hideModal }) => {
 
   const placement = document.querySelector(".App");
 
-  return isVisible ? createPortal(<div>{/* Register and login components here! */}</div>, placement) : null
+  return isVisible ? createPortal(<div className={styles.modalContainer}>{/* Register and login components here! */}</div>, placement) : null
 };
 
 export default RegisterLogInModal;
