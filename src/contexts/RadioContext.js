cd011cb0f16371838fc6a37b8e4a8dc308ceab6f
Christina <chrasse@hotmail.com>
2021-04-23 10:33:40 +0200

    Funcs to get channels, one channel and channel schedule

--- src/contexts/RadioContext.js
+++ src/contexts/RadioContext.js
@@ -6,11 +6,34 @@ export const RadioContext = createContext();
 const RadioContextProvider = (props) => {
   const history = useHistory();
 
+  const [allChannels, setAllChannels] = useState(null);
+  const [singleChannel, setSingleChannel] = useState(null);
 
+  const fetchAllChannels = async () => {
+    const allChannelsList = await fetch("/api/v1/channels/");
+    allChannelsList = await allChannelsList.json();
+    setAllChannels(allChannelsList);
+  };
 
+  useEffect(() => fetchAllChannels(), []);
+
+  const fetchSingleChannel = async (channelId) => {
+    let channelInfo = await fetch(`/api/v1/channels/${channelId}`);
+    channelInfo = await channelInfo.json();
+    setSingleChannel(channelInfo);
+  };
+
+  const fetchChannelSchedule = async (channelId) => {
+    let channelSchedule = await fetch(`/api/v1/channels/schedule/${channelId}`);
+    channelSchedule = await channelSchedule.json();
+    return channelSchedule;
+  };
 
   const values = {
-
+    allChannels,
+    singleChannel,
+    fetchSingleChannel,
+    fetchChannelSchedule,
   };
 
   return (
